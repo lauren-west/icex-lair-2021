57599e44f7296959f0c4b815f946d06f44a9338f
lauren-west <lwest@hmc.edu>
2021-06-17 15:39:11 -0700

    write to csv

--- sensor_data.py
+++ sensor_data.py
@@ -1,14 +1,16 @@
+import csv
+
 with open('CO_Preliminary_Test_Data.txt') as f:
     lines = f.readlines()
-
-my_list = []
 
 # data_list is 2D array of strings of data
 # rows are lines, and cols are the specific measurements
 data_list = []
-count = 0
 
 for line in lines:
     data_list.append(line.split(','))
 
-print(data_list)
+with open("output.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(data_list)
+
