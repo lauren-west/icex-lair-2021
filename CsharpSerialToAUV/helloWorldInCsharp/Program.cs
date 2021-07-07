5b395c09709527c1462dfbe59616a139a8b013d4
lauren-west <lwest@hmc.edu>
2021-07-07 11:51:06 -0700

    trying to create csharp project in repo
    trying to create csharp project in repo

---
+++ CsharpSerialToAUV/helloWorldInCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloWorldInCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // two ways to see print statement:
+            // 1) Debug, and press play.
+            // 2) cd to directory helloWorldInCsharp/bin/Debug/net5.0/
+            // then, enter command dotnet helloWorldInCsharp.dll
+        }
+    }
+}
