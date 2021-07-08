1adc8bbc9595c534b61feac85832806f46219da6
lauren-west <lwest@hmc.edu>
2021-07-08 12:14:58 -0700

    attempts to read serial port communication into visual studio

--- CsharpAUV/CsharpAUV/SerialDataHandler.cs
+++ CsharpAUV/CsharpAUV/SerialDataHandler.cs
@@ -1,34 +1,47 @@
 ﻿using System;
+using System.IO;
 using System.IO.Ports;
 
 namespace CsharpAUV
 {
     class SerialDataHandler
     {
-        int timeToRun = 30; // seconds
-        Tuple<double, double> tagGpsCoord =
-            new Tuple<double, double>(33.57676, -43.52746);
-        Tuple<double, double> sensorGpsCoord =
-            new Tuple<double, double>(0, 0);
+        //int timeToRun = 30; // seconds
+        //Tuple<double, double> tagGpsCoord =
+        //    new Tuple<double, double>(33.57676, -43.52746);
+        //Tuple<double, double> sensorGpsCoord =
+        //    new Tuple<double, double>(0, 0);
 
-        // constructor
-        SerialDataHandler()
-        {
-            // guess we are writing this later
-        }
+        
 
-        public void getSerialData()
-        {
-            /*
-            get_serial_data opens com port on PC in vs code
-            returns: None
-            */
-        }
+        //// constructor
+        //SerialDataHandler()
+        //{
+        //    // guess we are writing this later
+        //}
+
+        //public void getSerialData()
+        //{
+        //    /*
+        //    get_serial_data opens com port on PC in vs code
+        //    returns: None
+        //    */
+        //}
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hi Joan Caitlyn Hannah Roman!");
-            SerialDataHandler handler = new SerialDataHandler();
+            //SerialDataHandler handler = new SerialDataHandler();
+            SerialPort mySerialPort = new SerialPort("COM3", 9600);
+            try
+            {
+                mySerialPort.Open();
+                mySerialPort.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
