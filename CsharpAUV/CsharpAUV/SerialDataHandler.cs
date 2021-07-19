bc3703aef31f119fb5c313f278ad19c256067001
lauren-west <lwest@hmc.edu>
2021-07-19 11:55:50 -0700

    clean c# code
    make it easy for everyone to read code. Implemented prompts for important

--- CsharpAUV/CsharpAUV/SerialDataHandler.cs
+++ CsharpAUV/CsharpAUV/SerialDataHandler.cs
@@ -5,7 +5,7 @@ using System.Device.Location;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Collections;
-
+using System.Diagnostics;
 
 namespace CsharpAUV
 {
@@ -13,35 +13,21 @@ namespace CsharpAUV
     {
         static bool _continue;
         static SerialPort _serialPort;
+        public List<string> rawSerialData;
 
-        int timeToRun = 30; // seconds
-        Tuple<double, double> tagGpsCoord =
-            new Tuple<double, double>(33.57676, -43.52746);
-        Tuple<double, double> sensorGpsCoord =
-            new Tuple<double, double>(0, 0);
-        public List<string> outputList;
-        static double temp = 12; // (Celsius)
-        static double depth = 10; // (meters)
-        static double salinity = 33.5; // (ppt) Default: 33.5
-        // the mackenzie equation for speed of sound underwater
-        // http://resource.npl.co.uk/acoustics/techguides/soundseawater/content.html
-        double speedOfSound = 1448.96 + (4.591 * temp) -
-            (5.304 * Math.Pow(10,-2) * Math.Pow(temp,2)) +
-            (2.374 * Math.Pow(10,-4) * Math.Pow(temp,3)) +
-            (1.340 * (salinity-35)) + (1.630 * Math.Pow(10,-2) * depth) +
-            (1.675 * Math.Pow(10,-7)* Math.Pow(depth,2)) -
-            (1.025 * Math.Pow(10,-2)* temp * (salinity-35)) -
-            (7.139 * Math.Pow(10,-13)* temp * Math.Pow(depth,3));
+        double speedOfSound = calcSpeedOfSound();
+        static int timeToRun = getTimeToRun();
 
-        // constructor
+        Tuple<List<double>, List<DateTime>, List<string>> outputToParticleFilter;
+
         public SerialDataHandler()
         {
-            this.outputList = new List<string>();
+            this.rawSerialData = new List<string>();
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Joan Caitlyn Hannah Roman!");
-
+            Console.WriteLine("Welcome.");
             string name;
             string message;
             StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
@@ -49,43 +35,28 @@ namespace CsharpAUV
             SerialDataHandler serialdatahandler = new SerialDataHandler();
 
             // Create a new SerialPort object with default settings.
-
-            SerialPort _serialPort = new SerialPort("COM3", 9600);
-
-            // Allow the user to set the appropriate properties.
-            _serialPort.PortName = SetPortName(_serialPort.PortName);
-            _serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate);
-            _serialPort.Parity = SetPortParity(_serialPort.Parity);
-            _serialPort.DataBits = SetPortDataBits(_serialPort.DataBits);
-            _serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
-            _serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
-
-            // get Distance w/ C# equivalent to python's geopy
-            //var myLocation = new GeoCoordinate(-51.39792, -0.12084);
-            //var yourLocation = new GeoCoordinate(-29.83245, 31.04034);
-            //double distance = myLocation.GetDistanceTo(yourLocation);
-
+            _serialPort = new SerialPort("COM3", 9600);
 
             //Set the read / write timeouts
             _serialPort.ReadTimeout = 500;
             _serialPort.WriteTimeout = 500;
 
+            Console.WriteLine("Beginning to listen to " + _serialPort.PortName + ".");
             _serialPort.Open();
             _continue = true;
             readThread.Start();
             
-
             Console.Write("Name: ");
             name = Console.ReadLine();
 
-            Console.WriteLine("Type QUIT to exit");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 
             while (_continue)
             {
                 message = Console.ReadLine();
-                serialdatahandler.outputList.Add(message);
 
-                if (stringComparer.Equals("quit", message))
+                if (sw.ElapsedMilliseconds < timeToRun)
                 {
                     _continue = false;
                 }
@@ -99,18 +70,22 @@ namespace CsharpAUV
             readThread.Join();
             _serialPort.Close();
 
-            // start using data
+            // start using data (datetimes, transmitterIDs)
             Tuple<List<DateTime>, List<string>> data = serialdatahandler.makeData();
-            // retrieve totalTimes Lsit and timeOfFlight List from dateTimes List
+
             var (totalTime, timeOfFlight) = serialdatahandler.makeTimeOfFlightList(data.Item1);
 
-            List<double> distances = serialdatahandler.getDistFromTOF(timeOfFlight);
-            // TODO: hand over predicted distances to the particle filter???
+            List<double> distances = serialdatahandler.calcDistFromTOF(timeOfFlight);
+            
+            // outputToParticleFilter = distances, datetimes, transmitterID
+            serialdatahandler.outputToParticleFilter = Tuple.Create(distances, data.Item1, data.Item2);
 
         }
 
-        public List<double> getDistFromTOF(List<double> timeOfFlight)
-        {
+        public List<double> calcDistFromTOF(List<double> timeOfFlight)
+        {  /* param: list of timeOfFlight 
+            * returns: list of distances
+            */
             // getting predicted distance from TOF
             List<double> distances = new List<double>();
             for (int i = 0; i < timeOfFlight.Count; i++)
@@ -120,7 +95,12 @@ namespace CsharpAUV
             return distances;
         }
 
-            public Tuple<List<double>, List<double>> makeTimeOfFlightList(List<DateTime> dateTimes) {
+        public Tuple<List<double>, List<double>> makeTimeOfFlightList(List<DateTime> dateTimes)
+        {   /* 
+             * param: list of dateTimes 
+             * returns: list of totalTime and list of timeOfFlight
+             */
+
             List<double> totalTime = new List<double>();
             List<double> timeOfFlight = new List<double>();
             DateTime initialTime = dateTimes[0];
@@ -129,24 +109,25 @@ namespace CsharpAUV
                 totalTime.Add(diff1);
                 timeOfFlight.Add(diff1 % 8.179); // add total time % 8.179 to get tof
             }
+
             return Tuple.Create(totalTime, timeOfFlight);
         }
 
         public Tuple<List<DateTime>, List<string>> makeData()
-        {
-            //string dateString = "yyyy-MM-dd HH:mm:ss.fff";
+        {   /* 
+             * Using raw serial data, we isolate dateTimes and transmitterIDs
+             * returns: list of dateTimes and list of transmitterID
+             */
+
             List<DateTime> dateTimes = new List<DateTime>();
             List<string> transmitterID = new List<string>();
 
-            // split up the outputList
-            foreach (string line in outputList) {
+            foreach (string line in rawSerialData) {
                 string[] tempArr = line.Split();
                 if (tempArr.Length <= 10)
                 {
                     transmitterID.Add(tempArr[4]);
                     dateTimes.Add(DateTimeOffset.Parse(tempArr[2]).UtcDateTime);
-                    // TRY # 2 if above line doesn't work as desired.
-                    //dateTimes[line] = DateTime.ParseExact(tempArray[2], dateString, CultureInfo.InvariantCulture);
                 }
             }
             ArrayList DataList = new ArrayList();
@@ -154,8 +135,50 @@ namespace CsharpAUV
             DataList.Add(transmitterID);
 
             return Tuple.Create(dateTimes, transmitterID);
-
         }
+
+        public static double calcSpeedOfSound() {
+            /* 
+             * Prompts salinity, temperature, and depth quantities
+             * returns speed of sound
+             */
+
+            Console.WriteLine("Enter temperature (Celsius): Default=12");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter depth (meters): Default=10");
+            double depth = Convert.ToDouble(Console.ReadLine()); ;
+            Console.WriteLine("Enter salinity (ppt): Default=33.5");
+            double salinity = Convert.ToDouble(Console.ReadLine());
+
+            // the mackenzie equation for speed of sound underwater
+            // http://resource.npl.co.uk/acoustics/techguides/soundseawater/content.html
+
+            double speedOfSound = 1448.96 + (4.591 * temp) -
+                (5.304 * Math.Pow(10, -2) * Math.Pow(temp, 2)) +
+                (2.374 * Math.Pow(10, -4) * Math.Pow(temp, 3)) +
+                (1.340 * (salinity - 35)) + (1.630 * Math.Pow(10, -2) * depth) +
+                (1.675 * Math.Pow(10, -7) * Math.Pow(depth, 2)) -
+                (1.025 * Math.Pow(10, -2) * temp * (salinity - 35)) -
+                (7.139 * Math.Pow(10, -13) * temp * Math.Pow(depth, 3));
+
+            return speedOfSound;
+        }
+
+        public static int getTimeToRun()
+        {   /* 
+             * Prompts for a time to run in minutes,
+             * returns time to run in milliseconds
+             */
+
+            Console.WriteLine("Enter time to run program (minutes): ");
+            int timeToRun = int.Parse(Console.ReadLine());
+
+            // convert minutes to milliseconds
+            timeToRun = 60000 * timeToRun;
+
+            return timeToRun;
+        }
+
 
         public static void Read()
         {
@@ -169,6 +192,7 @@ namespace CsharpAUV
                 catch (TimeoutException) { }
             }
         }
+
 
         // Display Port values and prompt user to enter a port.
         public static string SetPortName(string defaultPortName)
@@ -205,7 +229,6 @@ namespace CsharpAUV
 
             return int.Parse(baudRate);
         }
-
         // Display PortParity values and prompt user to enter a value.
         public static Parity SetPortParity(Parity defaultPortParity)
         {
@@ -229,7 +252,7 @@ namespace CsharpAUV
         }
         // Display DataBits values and prompt user to enter a value.
         public static int SetPortDataBits(int defaultPortDataBits)
-        {
+        {   
             string dataBits;
 
             Console.Write("Enter DataBits value (Default: {0}): ", defaultPortDataBits);
@@ -242,7 +265,6 @@ namespace CsharpAUV
 
             return int.Parse(dataBits.ToUpperInvariant());
         }
-
         // Display StopBits values and prompt user to enter a value.
         public static StopBits SetPortStopBits(StopBits defaultPortStopBits)
         {
@@ -285,6 +307,29 @@ namespace CsharpAUV
 
             return (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
         }
-        
     }
 }
+
+
+// Have not used:
+//Tuple<double, double> tagCoord =
+//    new Tuple<double, double>(33.57676, -43.52746);
+//Tuple<double, double> sensorCoord =
+//    new Tuple<double, double>(0, 0);
+
+// get Distance w/ C# equivalent to python's geopy
+//var myLocation = new GeoCoordinate(-51.39792, -0.12084);
+//var yourLocation = new GeoCoordinate(-29.83245, 31.04034);
+//double distance = myLocation.GetDistanceTo(yourLocation);
+
+// deviate from default serialport settings
+// More info
+// @ https://docs.microsoft.com/en-us/dotnet/api/system.io.ports.serialport?view=dotnet-plat-ext-5.0
+//SerialPort _serialPort = new SerialPort();
+// Allow the user to set the appropriate properties.
+//_serialPort.PortName = SetPortName(_serialPort.PortName);
+//_serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate);
+//_serialPort.Parity = SetPortParity(_serialPort.Parity);
+//_serialPort.DataBits = SetPortDataBits(_serialPort.DataBits);
+//_serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
+//_serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
