8353d3a43ed8881921f66b62b1505313dc776fd3
lauren-west <lwest@hmc.edu>
2021-07-30 20:58:49 -0700

    clean up simulator
    - also threw live serial data collection code back in

--- CsharpAUV/CsharpAUV/Simulator.cs
+++ CsharpAUV/CsharpAUV/Simulator.cs
@@ -15,21 +15,27 @@ namespace CsharpAUV
 
             while (currentTime < finalTime)
             {
+                // Step 1: Get Measurements
                 List<Tuple<double, DateTime, string, string>> measurements = handler.getMeasurements(currentTime);
-
-                // run pf to estimate shark state
-
-                // plan based on shark state
-
-                // control
-                foreach (Tuple<double, DateTime, string, string> item in measurements) {
-
+                foreach (Tuple<double, DateTime, string, string> item in measurements)
+                {
+                    Console.WriteLine(item.Item1);
                     Console.WriteLine(item.Item2);
-
+                    Console.WriteLine(item.Item3);
+                    Console.WriteLine(item.Item4);
+                    Console.WriteLine();
                 }
+                Console.WriteLine();
+
+                // Step 2: Run pf to estimate shark state
+
+                // Step 3: Plan based on shark state
+
+                // Step 4: Control
 
                 currentTime = currentTime.AddSeconds(1);
             }
+
             Console.WriteLine("Done");
         }
     }
