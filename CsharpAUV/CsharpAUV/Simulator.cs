f01aaeb0c6d5bf459c7eee24e8b5f0b1b6a54d82
lauren-west <lwest@hmc.edu>
2021-08-16 10:26:28 -0700

    one at a time measurements!

--- CsharpAUV/CsharpAUV/Simulator.cs
+++ CsharpAUV/CsharpAUV/Simulator.cs
@@ -18,17 +18,42 @@ namespace CsharpAUV
                 List<Tuple<double, DateTime, int, int, double, double>> measurements = handler.getMeasurements1(currentTime);
                 Console.WriteLine("current time: {0}",
                            currentTime.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
+                if (measurements != null)
+                {
+                    // hand over to particle filter
+                    foreach (Tuple<double, DateTime, int, int, double, double> item in measurements)
+                    {
 
-                //foreach (Tuple<double, DateTime, int, int, double, double> item in measurements)
-                //{
-                //    Console.WriteLine(item.Item1);
-                //    Console.WriteLine("grabbed time: {0}",
-                //           item.Item2.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
-                //    Console.WriteLine(item.Item3);
-                //    Console.WriteLine(item.Item4);
-                //    Console.WriteLine(item.Item5);
-                //    Console.WriteLine(item.Item6);
-                //}
+                        //Console.WriteLine(item.Item1);
+                        Console.WriteLine("grabbed time: {0}",
+                               item.Item2.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
+                        //Console.WriteLine(item.Item3);
+                        //Console.WriteLine(item.Item4);
+                        //Console.WriteLine(item.Item5);
+                        //Console.WriteLine(item.Item6);
+                    }
+                    Console.WriteLine();
+                }
+                else {
+                     Console.WriteLine("measurements null");
+                }
+               
+
+                // if new measurement
+                // hand measurement over
+                // else
+                // not measurement
+                // best way to do this?
+                //create a member variable in handler named
+                // previousPingDT
+                // if dt in measurements is the same as the previousPingDT
+                //  this is NOT new data, do not give to particle filter
+                // else:
+                //  new data, hand it over!
+
+
+
+
                 //Console.WriteLine();
 
 
